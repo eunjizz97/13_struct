864c6cfdefa9d98fdfeb127cd3eb8c84034aecb7
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-29 23:43:28 +0900

    keyword.c

--- keyword.c
+++ keyword.c
@@ -4,6 +4,10 @@
 
 #define NKEYS       13
 
+struct keytab{
+	int num;
+	char keyword[10];
+}; 
 
 struct keytab keywrds[NKEYS] = {
        {0, "int"},
@@ -22,10 +26,22 @@ struct keytab keywrds[NKEYS] = {
 };
 
 void count_word(char* word){
+	int i;
 	
+	for(i=0;i<NKEYS;i++){
+		if(strncmp(word, keywrds[i].keyword, strlen(keywrds[i].keyword))== 0)
+		{
+			(keywrds[i].num)++;
+			break;
+		}
+	}
 }
 
 
 void print_word(void) {
+	int i;
 	
+	for(i=0;i<NKEYS;i++){
+		printf("%s : %d °³ \n", keywrds[i].keyword, keywrds[i].num);
+	}
 }
--- main.c
+++ main.c
@@ -76,8 +76,10 @@ int main(int argc, char *argv[]) {
 	while(fget_word(fp, word) != 0)
 	{
 		//word pricessing
-		printf("%s\n", word);	//test code
+		count_word(word);
 	}
+	
+	print_word();
 	
 	//fclose
 	fclose(fp);
